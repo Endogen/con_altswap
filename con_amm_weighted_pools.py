7564f8d459d28860cbbf39ea0482da0d1f49a944
Endogen <Endogen@users.noreply.github.com>
2022-07-27 02:22:21 +0200

    Update con_amm_weighted_pools.py
    Reworked whole contract

--- con_amm_weighted_pools.py
+++ con_amm_weighted_pools.py
@@ -1,14 +1,13 @@
 I = importlib
-token_interface = [I.Func('transfer', args=('amount', 'to')), I.Func(
-    'approve', args=('amount', 'to')), I.Func('transfer_from', args=(
-    'amount', 'to', 'main_account'))]
 
-pools = Hash(default_value=False)
-pools_creation_values = Hash(default_value=False)
-pool_weights = Hash(default_value=False)
-lp_tokens_users = Hash(default_value=0.0)
-lp_tokens_total = Hash(default_value=0.0)
-lp_tokens_approvals = Hash(default_value=0.0)
+token_interface = [
+    I.Func('transfer', args=('amount', 'to')), 
+    I.Func('approve', args=('amount', 'to')), 
+    I.Func('transfer_from', args=('amount', 'to', 'main_account'))
+]
+
+pools = Hash(default_value=0.0)
+lp_tokens = Hash(default_value=0.0)
 
 fee = Variable()
 operator = Variable()
@@ -19,138 +18,117 @@ def seed():
     operator.set("ff61544ea94eaaeb5df08ed863c4a938e9129aba6ceee5f31b6681bdede11b89")
 
 @export
-def createWeightedPool(poolName: str, assets: dict, weights: dict):
-    # assets {"currency": 2, "con_mineit": 1, "con_lusd_lst001": 1}
-    # weigths {"currency":0.5, "con_mineit": 0.25, "con_lusd_lst001":0.25}
-    assert pools[poolName] == False, "A pool with this name already exists"
-    sum_assets = 0
-    for contract, amount in assets.items():
-        assert amount > 0, "Amounts need to be more then 0"
-        sum_assets += amount
-    for contract, amount in assets.items():
-        assert amount == sum_assets / 100 * (weights[contract] * 100), "Token Amount and Weights are not matching"
+def createWeightedPool(poolName: str, assets: dict):
+    assert not pools[poolName], "A pool with this name already exists"
+    
+    sum_assets = sum(assets.values())
+
     for contract, amount in assets.items():
         token = I.import_module(contract)
-        assert I.enforce_interface(token, token_interface
-        ), f'Invalid token interface for {contract}!'
-        token.transfer_from(amount=amount, to=ctx.this, main_account=ctx.caller)
+
+        assert amount > 0, f"Amount of {contract} need to be more than 0"
+        assert I.enforce_interface(token, token_interface), f'Invalid token interface for {contract}!'
+        
+        token.transfer_from(amount=amount, to=ctx.this, main_account=ctx.caller) 
+
         pools[poolName, contract] = amount
-        pools_creation_values[poolName, contract] = amount
-        pool_weights[poolName, contract] = weights[contract]
-    lp_tokens_users[poolName, ctx.caller] = 100
-    lp_tokens_total[poolName] = 100
+        pools[poolName, contract, 'init'] = amount
+        pools[poolName, contract, 'weight'] = amount / sum_assets
 
-    return poolName
+    lp_tokens[poolName] = 100
+    lp_tokens[poolName, ctx.caller] = 100
 
+    pools[poolName] = list(assets.keys())
 
 @export
 def addLiquidity(poolName: str, assets: dict):
-    sum_assets_deposit = 0
-    sum_assets = 0
-    for contract, amount in assets.items():
-        assert amount > 0, "Amounts need to be more then 0"
-        sum_assets_deposit += amount
-        sum_assets += pools_creation_values[poolName, contract]
-    for contract,amount in assets.items():
-        assert amount == sum_assets_deposit / 100 * (pool_weights[poolName, contract] * 100), "Token Amount and Weights are not matching"
+    sum_assets = sum(assets.values())
+
     for contract, amount in assets.items():
         token = I.import_module(contract)
-        assert I.enforce_interface(token, token_interface
-        ), f'Invalid token interface for {contract}!'
+
+        assert amount == sum_assets / 100 * (pools[poolName, contract, 'weight'] * 100), f"Amount and weights for {contract} not matching"
+        assert I.enforce_interface(token, token_interface), f'Invalid token interface for {contract}!'
+        assert amount > 0, f"Amount of {contract} need to be more than 0"
+
         token.transfer_from(amount=amount, to=ctx.this, main_account=ctx.caller)
-        pools[poolName, contract] += amount
         
-    total_lp_points = lp_tokens_total[poolName]
-    lp_to_mint = (sum_assets_deposit / sum_assets) * 100
-    lp_tokens_total[poolName] += lp_to_mint
-    lp_tokens_users[poolName, ctx.caller] += lp_to_mint
+        pools[poolName, contract] += amount
+        sum_assets_init += pools[poolName, contract, 'init']
+        
+    lp_to_mint = (sum_assets / sum_assets_init) * 100
 
+    lp_tokens[poolName] += lp_to_mint
+    lp_tokens[poolName, ctx.caller] += lp_to_mint
+    
     return lp_to_mint
-
 
 @export
 def removeLiquidity(poolName: str, amountLPTokens: float):
-    assert amountLPTokens <= lp_tokens_users[poolName, ctx.caller], "You don't own that many LP points"
-    assert lp_tokens_total[poolName] - amountLPTokens > 1, "You need to leave atleast 1 LP point"
-    assert amountLPTokens > 0, "Needs to more then 0"
-    total_lp_points = lp_tokens_total[poolName]
-    for contract in pools[poolName].all():
-        amount = pools[poolName, contract] / total_lp_points * amountLPTokens
-        token.transfer(amount=amount, to=ctx.caller)
+    assert amountLPTokens <= lp_tokens[poolName, ctx.caller], "You don't own that many LP points"
+    assert lp_tokens[poolName] - amountLPTokens > 1, "You need to leave atleast 1 LP point"
+    assert amountLPTokens > 0, "Amount needs to be more than 0"
+    
+    for contract in pools[poolName]:
+        amount = pools[poolName, contract] / lp_tokens[poolName] * amountLPTokens
+        
+        I.import_module(contract).transfer(amount=amount, to=ctx.caller)
+        
         pools[poolName, contract] -= amount
-        lp_tokens_total[poolName] -= amountLPTokens
-        lp_tokens_users[poolName, ctx.caller] -= amountLPTokens
-
+        lp_tokens[poolName] -= amountLPTokens
+        lp_tokens[poolName, ctx.caller] -= amountLPTokens
 
 @export
 def transferLiquidity(poolName: str, amountLPTokens: float, to: str):
-    assert amountLPTokens <= lp_tokens_users[poolName, ctx.caller], "You don't own that many LP points"
-    assert amountLPTokens > 0, "Needs to more then 0"
-    lp_tokens_users[poolName, ctx.caller] -= amountLPTokens
-    lp_tokens_users[poolName, to] += amountLPTokens
+    assert amountLPTokens <= lp_tokens[poolName, ctx.caller], "You don't own that many LP points"
+    assert amountLPTokens > 0, "Amount needs to be more than 0"
+
+    lp_tokens[poolName, to] += amountLPTokens
+    lp_tokens[poolName, ctx.caller] -= amountLPTokens
 
 @export
 def approveLiquidity(poolName: str, amountLPTokens: float, main_account: str, to: str):
-   assert amountLPTokens <= lp_tokens_users[poolName, ctx.caller], "You don't own that many LP points"
-   assert amountLPTokens > 0, "Needs to more then 0"
-   lp_tokens_approvals[poolName, ctx.caller, to] += amountLPTokens
+    assert amountLPTokens <= lp_tokens[poolName, ctx.caller], "You don't own that many LP points"
+    assert amountLPTokens > 0, "Amount needs to be more than 0"
+
+    lp_tokens[poolName, ctx.caller, to] += amountLPTokens
 
 @export
 def transferLiquidityFrom(poolName: str, amountLPTokens: float, main_account: str, to: str):
-    assert amountLPTokens <= lp_tokens_users[poolName, main_account], "Not enough LP points to send"
-    assert amountLPTokens > 0, "Needs to more then 0"
-    lp_tokens_users[poolName, main_account] -= amountLPTokens
-    lp_tokens_users[poolName, to] += amountLPTokens
-    lp_tokens_approvals[poolName, main_account, to] -= amountLPTokens
-
+    assert amountLPTokens <= lp_tokens[poolName, main_account], "Not enough LP points to send"
+    assert lp_tokens[poolName, main_account, to] >= amountLPTokens, "Not enough LP points approved to send"
+    assert amountLPTokens > 0, "Amount needs to be more than 0"
+    
+    lp_tokens[poolName, to] += amountLPTokens
+    lp_tokens[poolName, main_account] -= amountLPTokens
+    lp_tokens[poolName, main_account, to] -= amountLPTokens
 
 @export
 def swap(poolName: str, amountFrom: float, contractFrom: str, contractTo: str):
-    assert amountFrom > 0, "Needs to more then 0"
-    fee_amount = amountFrom/100*fee.get()
+    assert amountFrom > 0, "Amount needs to be more than 0"
+
+    fee_amount = amountFrom / 100 * fee.get()
     swap_amount = amountFrom - fee_amount
+    
     local_balances = [pools[poolName, contractFrom], pools[poolName, contractTo]]
-    local_weights = [pool_weights[poolName, contractFrom], pool_weights[poolName, contractTo]]
-    final_amount = calc_out_given_in(local_balances[0], local_weights[0], local_balances[1], local_weights[1], swap_amount)       
-    fromToken = I.import_module(contractFrom)   
-    toToken =  I.import_module(contractTo)   
-    fromToken.transfer_from(amount=swap_amount, to=ctx.this, main_account=ctx.caller)
-    fromToken.transfer_from(amount=fee_amount, to=operator.get(), main_account=ctx.caller)
+    local_weights = [pools[poolName, contractFrom, 'weight'], pools[poolName, contractTo, 'weight']]
+
+    denominator = local_balances[0] + swap_amount
+    base = local_balances[0] / denominator
+    exponent = local_weights[0] / local_weights[1]
+    power = base ** exponent
+
+    final_amount = local_balances[1] * ((1 - power) if power < 1 else 0)
+    
+    I.import_module(contractFrom).transfer_from(amount=swap_amount + fee_amount, to=ctx.this, main_account=ctx.caller)
+    I.import_module(contractTo).transfer(amount=final_amount, to=ctx.caller)
+
     pools[poolName, contractFrom] += swap_amount
-    toToken.transfer(amount=final_amount, to= ctx.caller)
     pools[poolName, contractTo] -= swap_amount
+    
     return final_amount
 
-    
-def calc_out_given_in(balance_in: decimal,
-                          weight_in: decimal,
-                          balance_out: decimal,
-                          weight_out: decimal,
-                          amount_in: decimal):
-
-    denominator = balance_in + amount_in
-    base = div(balance_in, denominator)  # balanceIn.divUp(denominator);
-    exponent = div(weight_in, weight_out)  # weightIn.divDown(weightOut);
-    power = powerIt(base, exponent)  # base.powUp(exponent);
-
-    return mul(balance_out, complement(power))  # balanceOut.mulDown(power.complement());
-
-
-def mul(a: decimal, b: decimal):
-    return a*b
-
-
-def div(a: decimal, b: decimal):
-    result =  a/b
-    return result
-
-
-def complement(a: decimal):
-    return (1 - a) if a < 1 else 0
-
-
-def powerIt(a: decimal,b:decimal):
-    return a**b
-
-
-
+@export
+def adjustFee(feeInPercent: float):
+    assert ctx.caller == operator.get(), "Only executable by operator"
+    fee.set(feeInPercent)
