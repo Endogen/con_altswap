4a24ac6c6ceefd76672affeb9876ff50e00edc9e
Endogen <Endogen@users.noreply.github.com>
2022-08-10 02:50:38 +0200

    Changed total amount of wallet balance

--- tests/currency.py
+++ tests/currency.py
@@ -2,7 +2,7 @@ balances = Hash(default_value=0)
 
 @construct
 def seed():
-    balances['ae7d14d6d9b8443f881ba6244727b69b681010e782d4fe482dbfb0b6aca02d5d'] = 10000000000
+    balances['ae7d14d6d9b8443f881ba6244727b69b681010e782d4fe482dbfb0b6aca02d5d'] = 10000
 
 @export
 def transfer(amount: float, to: str):
